2cc628d29e3b8b5521851c722ceef4ff368066d6
porneL <pornel@pornel.net>
2011-03-07 23:31:10 +0000

    Use gamma 2.2 internally

--- pam.h
+++ pam.h
@@ -24,12 +24,16 @@ typedef struct {
     float r, g, b, a;
 } f_pixel;
 
+static const float internal_gamma = 0.45455;
+
 /*
- Converts 8-bit RGB to linear RGB premultiplied by alpha.
+ Converts 8-bit RGB with given gamma to scalar RGB with internal gamma and premultiplied by alpha.
  Premultiplied color space is much better for blending of semitransparent colors.
  */
 inline static f_pixel to_f(float gamma, rgb_pixel px)
 {
+    gamma /= internal_gamma;
+
     float r = powf(px.r/255.0f, 1.0f/gamma),
           g = powf(px.g/255.0f, 1.0f/gamma),
           b = powf(px.b/255.0f, 1.0f/gamma),
@@ -45,6 +49,8 @@ inline static rgb_pixel to_rgb(float gamma, f_pixel px)
     }
 
     float r,g,b,a;
+
+    gamma /= internal_gamma;
 
     // 256, because numbers are in range 1..255.9999… rounded down
     r = powf(px.r/px.a, gamma)*256.0f;

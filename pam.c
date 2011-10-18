3b76ae3efbf8593a294d89cfc3b1c52bde252c9e
porneL <pornel@pornel.net>
2011-10-18 22:25:23 +0100

    Removed problematic u_register_t typedef

--- pam.c
+++ pam.c
@@ -19,14 +19,6 @@
 
 #include "pam.h"
 
-#ifndef __FreeBSD__
-#ifdef __LP64__
-typedef unsigned long long u_register_t;
-#else
-typedef unsigned long u_register_t;
-#endif
-#endif
-
 #define PAM_EQUAL(p,q) ((p).a == (q).a && (p).r == (q).r && (p).g == (q).g && (p).b == (q).b)
 
 static hist *pam_acolorhashtoacolorhist(acolorhash_table acht, int maxacolors);

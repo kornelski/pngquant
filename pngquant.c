c66182d653a47f9a2c8d4a3f1a1f399686b2fe04
porneL <pornel@pornel.net>
2011-10-29 14:13:45 +0100

    Version 1.6

--- pngquant.c
+++ pngquant.c
@@ -22,7 +22,7 @@
                 specify a mapfile, switch to grayscale */
 /* GRR TO DO:  if all samples are 0 or maxval, eliminate gAMA chunk (rwpng.c) */
 
-#define PNGQUANT_VERSION "1.5.1 (September 2011)"
+#define PNGQUANT_VERSION "1.6 (October 2011)"
 
 #define PNGQUANT_USAGE "\
    usage:  pngquant [options] [ncolors] [pngfile [pngfile ...]]\n\n\

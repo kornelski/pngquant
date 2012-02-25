76ccd8999c26e42a3e5ae38e9e54ba2ec5d6272c
porneL <pornel@pornel.net>
2012-02-25 12:10:04 +0000

    Don't modify input pixels unless working around IE6 bug

--- pngquant.c
+++ pngquant.c
@@ -689,7 +689,7 @@ static histogram *get_histogram(const read_info *input_image, const int reqcolor
     return hist;
 }
 
-static float modify_alpha(read_info *input_image, const float min_opaque_val)
+static void modify_alpha(read_info *input_image, const float min_opaque_val)
 {
     /* IE6 makes colors with even slightest transparency completely transparent,
        thus to improve situation in IE, make colors that are less than ~10% transparent
@@ -698,48 +698,29 @@ static float modify_alpha(read_info *input_image, const float min_opaque_val)
     rgb_pixel *const *const input_pixels = (rgb_pixel **)input_image->row_pointers;
     const int rows= input_image->height, cols = input_image->width;
     const float gamma = input_image->gamma;
-    float almost_opaque_val;
 
-    if (min_opaque_val < 1.f) {
-        almost_opaque_val = min_opaque_val * 169.f/256.f;
-        verbose_printf("  Working around IE6 bug by making image less transparent...\n");
-    } else {
-        almost_opaque_val = 1;
-    }
+    if (min_opaque_val > 254.f/255.f) return;
+
+    const float almost_opaque_val = min_opaque_val * 169.f/256.f;
+    const int almost_opaque_val_int = almost_opaque_val*255.f;
+
+    verbose_printf("  Working around IE6 bug by making image less transparent...\n");
 
     for(int row = 0; row < rows; ++row) {
         for(int col = 0; col < cols; col++) {
             const rgb_pixel srcpx = input_pixels[row][col];
-            f_pixel px = to_f(gamma, srcpx);
 
-#ifndef NDEBUG
-            rgb_pixel rgbcheck = to_rgb(gamma, px);
-
-            if (srcpx.a && (srcpx.r != rgbcheck.r || srcpx.g != rgbcheck.g || srcpx.b != rgbcheck.b || srcpx.a != rgbcheck.a)) {
-                fprintf(stderr, "Conversion error: expected %d,%d,%d,%d got %d,%d,%d,%d\n",
-                        srcpx.r,srcpx.g,srcpx.b,srcpx.a, rgbcheck.r,rgbcheck.g,rgbcheck.b,rgbcheck.a);
-                return -1;
-            }
-#endif
-            /* set all completely transparent colors to black */
-            if (srcpx.a < 255) {
-                if (!srcpx.a) {
-                    input_pixels[row][col] = (rgb_pixel){0,0,0,0};
-            }
-                else if (px.a > almost_opaque_val) {
             /* ie bug: to avoid visible step caused by forced opaqueness, linearily raise opaqueness of almost-opaque colors */
-                assert((min_opaque_val-almost_opaque_val)>0);
+            if (srcpx.a >= almost_opaque_val_int) {
+                f_pixel px = to_f(gamma, srcpx);
 
                 float al = almost_opaque_val + (px.a-almost_opaque_val) * (1-almost_opaque_val) / (min_opaque_val-almost_opaque_val);
                 if (al > 1) al = 1;
                 px.a = al;
-                    input_pixels[row][col].a = to_rgb(gamma, px).a;
+                input_pixels[row][col] = to_rgb(gamma, px);
             }
         }
     }
-    }
-
-    return min_opaque_val;
 }
 
 static pngquant_error read_image(const char *filename, int using_stdin, read_info *input_image_p)

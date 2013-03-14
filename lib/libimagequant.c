92b05755a4ff7f71f3c420de86f8a448b7dc1525
Kornel Lesiński <kornel@geekhood.net>
2013-03-14 00:30:33 +0000

    Warn that C99 is required.

--- lib/libimagequant.c
+++ lib/libimagequant.c
@@ -19,6 +19,11 @@
 #include <stdarg.h>
 #include <stdbool.h>
 
+#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199900L
+#error "This program requires C99, e.g. -std=c99 switch in GCC. MSVC doesn't support C newer than '89, please use MinGW on Windows."
+#error "Ignore torrent of syntax errors that may follow. It's only because compiler is set to use too old C version."
+#endif
+
 #ifdef _OPENMP
 #include <omp.h>
 #else

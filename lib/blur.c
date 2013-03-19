49e34a6a7b88ef65272f749df38aa86ca7d4514c
Kornel Lesiński <kornel@geekhood.net>
2013-03-19 00:07:50 +0000

    Blur using uchar rather than float

--- lib/blur.c
+++ lib/blur.c
@@ -5,16 +5,14 @@
 /*
  Blurs image horizontally (width 2*size+1) and writes it transposed to dst (called twice gives 2d blur)
  */
-static void transposing_1d_blur(float *restrict src, float *restrict dst, unsigned int width, unsigned int height, const unsigned int size)
+static void transposing_1d_blur(unsigned char *restrict src, unsigned char *restrict dst, unsigned int width, unsigned int height, const unsigned int size)
 {
-    const float sizef = size;
-
     for(unsigned int j=0; j < height; j++) {
-        float *restrict row = src + j*width;
+        unsigned char *restrict row = src + j*width;
 
         // accumulate sum for pixels outside line
-        float sum;
-        sum = row[0]*sizef;
+        unsigned int sum;
+        sum = row[0]*size;
         for(unsigned int i=0; i < size; i++) {
             sum += row[i];
         }
@@ -24,14 +22,14 @@ static void transposing_1d_blur(float *restrict src, float *restrict dst, unsign
             sum -= row[0];
             sum += row[i+size];
 
-            dst[i*height + j] = sum / (sizef*2.f);
+            dst[i*height + j] = sum / (size*2);
         }
 
         for(unsigned int i=size; i < width-size; i++) {
             sum -= row[i-size];
             sum += row[i+size];
 
-            dst[i*height + j] = sum / (sizef*2.f);
+            dst[i*height + j] = sum / (size*2);
         }
 
         // blur with right side outside line
@@ -39,7 +37,7 @@ static void transposing_1d_blur(float *restrict src, float *restrict dst, unsign
             sum -= row[i-size];
             sum += row[width-1];
 
-            dst[i*height + j] = sum/(sizef*2.0f);
+            dst[i*height + j] = sum / (size*2);
         }
     }
 }
@@ -47,26 +45,26 @@ static void transposing_1d_blur(float *restrict src, float *restrict dst, unsign
 /**
  * Picks maximum of neighboring pixels (blur + lighten)
  */
-void max3(float *src, float *dst, unsigned int width, unsigned int height)
+void max3(unsigned char *src, unsigned char *dst, unsigned int width, unsigned int height)
 {
     for(unsigned int j=0; j < height; j++) {
-        const float *row = src + j*width,
+        const unsigned char *row = src + j*width,
         *prevrow = src + (j > 1 ? j-1 : 0)*width,
         *nextrow = src + MIN(height-1,j+1)*width;
 
-        float prev,curr=row[0],next=row[0];
+        unsigned char prev,curr=row[0],next=row[0];
 
         for(unsigned int i=0; i < width-1; i++) {
             prev=curr;
             curr=next;
             next=row[i+1];
 
-            float t1 = MAX(prev,next);
-            float t2 = MAX(nextrow[i],prevrow[i]);
+            unsigned char t1 = MAX(prev,next);
+            unsigned char t2 = MAX(nextrow[i],prevrow[i]);
             *dst++ = MAX(curr,MAX(t1,t2));
         }
-        float t1 = MAX(curr,next);
-        float t2 = MAX(nextrow[width-1],prevrow[width-1]);
+        unsigned char t1 = MAX(curr,next);
+        unsigned char t2 = MAX(nextrow[width-1],prevrow[width-1]);
         *dst++ = MAX(t1,t2);
     }
 }
@@ -74,26 +72,26 @@ void max3(float *src, float *dst, unsigned int width, unsigned int height)
 /**
  * Picks minimum of neighboring pixels (blur + darken)
  */
-void min3(float *src, float *dst, unsigned int width, unsigned int height)
+void min3(unsigned char *src, unsigned char *dst, unsigned int width, unsigned int height)
 {
     for(unsigned int j=0; j < height; j++) {
-        const float *row = src + j*width,
+        const unsigned char *row = src + j*width,
         *prevrow = src + (j > 1 ? j-1 : 0)*width,
         *nextrow = src + MIN(height-1,j+1)*width;
 
-        float prev,curr=row[0],next=row[0];
+        unsigned char prev,curr=row[0],next=row[0];
 
         for(unsigned int i=0; i < width-1; i++) {
             prev=curr;
             curr=next;
             next=row[i+1];
 
-            float t1 = MIN(prev,next);
-            float t2 = MIN(nextrow[i],prevrow[i]);
+            unsigned char t1 = MIN(prev,next);
+            unsigned char t2 = MIN(nextrow[i],prevrow[i]);
             *dst++ = MIN(curr,MIN(t1,t2));
         }
-        float t1 = MIN(curr,next);
-        float t2 = MIN(nextrow[width-1],prevrow[width-1]);
+        unsigned char t1 = MIN(curr,next);
+        unsigned char t2 = MIN(nextrow[width-1],prevrow[width-1]);
         *dst++ = MIN(t1,t2);
     }
 }
@@ -102,7 +100,7 @@ void min3(float *src, float *dst, unsigned int width, unsigned int height)
  Filters src image and saves it to dst, overwriting tmp in the process.
  Image must be width*height pixels high. Size controls radius of box blur.
  */
-void blur(float *src, float *tmp, float *dst, unsigned int width, unsigned int height, unsigned int size)
+void blur(unsigned char *src, unsigned char *tmp, unsigned char *dst, unsigned int width, unsigned int height, unsigned int size)
 {
     if (width < 2*size+1 || height < 2*size+1) return;
     transposing_1d_blur(src, tmp, width, height, size);
--- lib/blur.h
+++ lib/blur.h
@@ -1,4 +1,4 @@
 
-void blur(float *src, float *tmp, float *dst, unsigned int width, unsigned int height, unsigned int size);
-void max3(float *src, float *dst, unsigned int width, unsigned int height);
-void min3(float *src, float *dst, unsigned int width, unsigned int height);
+void blur(unsigned char *src, unsigned char *tmp, unsigned char *dst, unsigned int width, unsigned int height, unsigned int size);
+void max3(unsigned char *src, unsigned char *dst, unsigned int width, unsigned int height);
+void min3(unsigned char *src, unsigned char *dst, unsigned int width, unsigned int height);

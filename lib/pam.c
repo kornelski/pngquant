bca59f250f1bfb2432e20b86a5dc10a016473db2
Jacob Middag <jacobmiddag@voormedia.com>
2014-03-27 14:34:18 +0100

    Added braces around initializers

--- lib/pam.c
+++ lib/pam.c
@@ -47,7 +47,7 @@ LIQ_PRIVATE bool pam_computeacolorhash(struct acolorhash_table *acht, const rgba
 
             // RGBA color is casted to long for easier hasing/comparisons
             const rgba_pixel tmp = pixels[row][col];
-            union rgba_as_int px = {tmp.r, tmp.g, tmp.b, tmp.a};
+            union rgba_as_int px = {{tmp.r, tmp.g, tmp.b, tmp.a}};
             unsigned int hash;
             if (!px.rgba.a) {
                 // "dirty alpha" has different RGBA values that end up being the same fully transparent color
@@ -86,7 +86,7 @@ LIQ_PRIVATE bool pam_computeacolorhash(struct acolorhash_table *acht, const rgba
                     // the array was allocated with spare items
                     if (i < achl->capacity) {
                         other_items[i] = (struct acolorhist_arr_item){
-                            .color = {px.rgba.r, px.rgba.g, px.rgba.b, px.rgba.a},
+                            .color = {{px.rgba.r, px.rgba.g, px.rgba.b, px.rgba.a}},
                             .perceptual_weight = boost,
                         };
                         achl->used++;
@@ -128,7 +128,7 @@ LIQ_PRIVATE bool pam_computeacolorhash(struct acolorhash_table *acht, const rgba
                     achl->other_items = new_items;
                     achl->capacity = capacity;
                     new_items[i] = (struct acolorhist_arr_item){
-                        .color = {px.rgba.r, px.rgba.g, px.rgba.b, px.rgba.a},
+                        .color = {{px.rgba.r, px.rgba.g, px.rgba.b, px.rgba.a}},
                         .perceptual_weight = boost,
                     };
                     achl->used++;

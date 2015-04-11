8c84f98ec5656f3cf194b6da6a61129bb6c320c7
Kornel Lesiński <kornel@geekhood.net>
2015-04-11 23:18:28 +0100

    2.4.0

--- lib/libimagequant.h
+++ lib/libimagequant.h
@@ -9,8 +9,8 @@
 #define LIQ_EXPORT extern
 #endif
 
-#define LIQ_VERSION 20306
-#define LIQ_VERSION_STRING "2.3.6"
+#define LIQ_VERSION 20400
+#define LIQ_VERSION_STRING "2.4.0"
 
 #ifndef LIQ_PRIVATE
 #if defined(__GNUC__) || defined (__llvm__)
--- pngquant.spec
+++ pngquant.spec
@@ -1,5 +1,5 @@
 Name:           pngquant
-Version:        2.3.6
+Version:        2.4.0
 Release:        1%{?dist}
 Summary:        PNG quantization tool for reducing image file size
 License:        BSD

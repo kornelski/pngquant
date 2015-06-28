f3c2b45788885f1deee22e90e85c3d3d5b80f295
Kornel Lesiński <kornel@geekhood.net>
2015-06-28 23:52:06 +0100

    clang analyzer warnings

--- lib/blur.c
+++ lib/blur.c
@@ -13,6 +13,8 @@
  */
 static void transposing_1d_blur(unsigned char *restrict src, unsigned char *restrict dst, unsigned int width, unsigned int height, const unsigned int size)
 {
+    assert(size > 0);
+
     for(unsigned int j=0; j < height; j++) {
         unsigned char *restrict row = src + j*width;
 

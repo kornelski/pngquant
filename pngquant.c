503f8ffb6689ccf367d05243fb4a50ee7cf78f00
Kornel <kornel@geekhood.net>
2017-11-04 00:40:25 +0000

    Bump

--- pngquant.c
+++ pngquant.c
@@ -7,7 +7,7 @@
 ** See COPYRIGHT file for license.
 */
 
-#define PNGQUANT_VERSION LIQ_VERSION_STRING " (October 2017)"
+#define PNGQUANT_VERSION LIQ_VERSION_STRING " (November 2017)"
 
 #define PNGQUANT_USAGE "\
 usage:  pngquant [options] [ncolors] -- pngfile [pngfile ...]\n\
